fc8a479f0abd0c615f91f7059576b4941360b2dc
AlvaroLobato5 <luisalvaro664@gmail.com>
2023-05-01 18:15:31 -0300

    fix

--- src/main/java/com/alvaro/rpgmod/entity/custom/globlin/GloblinLordEntity.java
+++ src/main/java/com/alvaro/rpgmod/entity/custom/globlin/GloblinLordEntity.java
@@ -6,6 +6,13 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.level.Level;
+import software.bernie.geckolib.core.animatable.GeoAnimatable;
+import software.bernie.geckolib.core.animation.AnimatableManager.ControllerRegistrar;
+import software.bernie.geckolib.core.animation.Animation;
+import software.bernie.geckolib.core.animation.AnimationController;
+import software.bernie.geckolib.core.animation.AnimationState;
+import software.bernie.geckolib.core.animation.RawAnimation;
+import software.bernie.geckolib.core.object.PlayState;
 
 public class GloblinLordEntity extends AbstractGloblin{
 
@@ -27,6 +34,30 @@ public class GloblinLordEntity extends AbstractGloblin{
     protected void registerGoals() {
         super.registerGoals();
         this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.2D, false));
+    }
+    
+    @Override
+    public void registerControllers(ControllerRegistrar controllers) {
+        controllers.add(new AnimationController<>(this, "controller", this::predicate));
+        controllers.add(new AnimationController<>(this, "attackController", this::attackPredicate));
+    }
+
+    private <T extends GeoAnimatable> PlayState attackPredicate(AnimationState<T> state) {
+        if (this.swinging){
+            state.getController().forceAnimationReset();
+            state.getController().setAnimation(RawAnimation.begin().then("animation.globlin_lord.attack", Animation.LoopType.PLAY_ONCE));
+            this.updateSwingTime();
+        }
+        return PlayState.CONTINUE;
+    }
+    private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> tAnimationState){
+        
+        if (tAnimationState.isMoving()){
+            return tAnimationState.setAndContinue(RawAnimation.begin().thenLoop("animation.globlin_lord.walk"));
+        }
+        else{
+            return PlayState.STOP;
+        }
     }
 
 }
